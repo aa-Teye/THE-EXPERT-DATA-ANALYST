2a543d62f1f66809a88277b0bc8eea3357155d14
AWonders <alexteyeametepey@gmail.com>
2025-07-31 09:52:59 +0100

    Added multiply funtion

--- summation.py
+++ summation.py
@@ -3,3 +3,9 @@ def summation(num1, num2):
     return result
 
 print(f"The sum is {summation(20,30)}")
+
+def multiply(num1, num2):
+    result  = num1 * num2
+    return result 
+
+print(f"The product of the two number is {multiply(20,30)}")
