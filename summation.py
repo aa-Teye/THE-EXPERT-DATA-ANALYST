8b742f78a9836fb52b542e67ca9fb0d11dd75ed0
AWonders <alexteyeametepey@gmail.com>
2025-08-14 01:13:06 +0100

    First Portfolio website

--- summation.py
+++ summation.py
@@ -9,3 +9,12 @@ def multiply(num1, num2):
     return result 
 
 print(f"The product of the two number is {multiply(20,30)}")
+
+def isOdd(num):
+    if num % 2 != 0:
+        return True
+    else:
+        return False
+print(f"The number is odd: {isOdd(21)}")
+print(isOdd(23))
+
